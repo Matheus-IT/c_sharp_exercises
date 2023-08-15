8b5c75aadfc3087fa2b01d76246048cedf96597d
matheuscosta <matheuscosta.tech@gmail.com>
2023-08-15 20:55:35 -0300

    Using conditionals and if statements

--- numbers/Program.cs
+++ numbers/Program.cs
@@ -1,19 +1,10 @@
-﻿short min1 = short.MinValue;
-short max1 = short.MaxValue;
-Console.WriteLine($"The range is {min1} to {max1}");
+﻿int age = int.Parse(Console.ReadLine() ?? "");
 
-int min2 = int.MinValue;
-int max2 = int.MaxValue;
-Console.WriteLine($"The range is {min2} to {max2}");
+string msg;
 
-float min3 = float.MinValue;
-float max3 = float.MaxValue;
-Console.WriteLine($"The range is {min3} to {max3}");
+if (age >= 18)
+    msg = "Can drive";
+else
+    msg = "Cannot drive";
 
-double min4 = double.MinValue;
-double max4 = double.MaxValue;
-Console.WriteLine($"The range is {min4} to {max4}");
-
-decimal min5 = decimal.MinValue;
-decimal max5 = decimal.MaxValue;
-Console.WriteLine($"The range is {min5} to {max5}");
+Console.WriteLine(msg);
