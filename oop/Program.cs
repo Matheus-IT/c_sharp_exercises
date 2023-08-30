64ec4ae27c03cdaa9ad86f7d4d3cbc91e4321018
matheuscosta <matheuscosta.tech@gmail.com>
2023-08-29 23:18:19 -0300

    Trying things out...

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,3 +1,7 @@
 ﻿var account = new BankAccount("Matheus", 10000);
 account.MakeWithdrawal(50, "Hammock");
+try {
+    account.MakeDeposit(-150, "Stealing");
+} catch (ArgumentOutOfRangeException e) {}
+
 Console.WriteLine($"Account {account.Number} was created for {account.Owner} with ${account.Balance}");
